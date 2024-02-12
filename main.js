5453d75d46ed5404bfda58609a93841a1d896786
ezequielzabala400 <ezequielzabala400@gmail.com>
2024-02-12 12:20:14 -0300

    active-states

--- main.js
+++ main.js
@@ -6,14 +6,20 @@ const menuIcon = document.querySelector('.menu-icon');
 menuIcon.addEventListener("click", () => {
   navbarList.classList.remove("hide")
   closeIcon.classList.remove("hide")
+  navbarList.classList.add("show")
+  closeIcon.classList.add("show")
 })
 
 closeIcon.addEventListener("click", () => {
+  navbarList.classList.remove("show")
+  closeIcon.classList.remove("show")
   navbarList.classList.add("hide")
   closeIcon.classList.add("hide")
 })
 
 window.addEventListener("scroll", () => {
+  navbarList.classList.remove("show")
+  closeIcon.classList.remove("show")
   navbarList.classList.add("hide")
   closeIcon.classList.add("hide")
 })
